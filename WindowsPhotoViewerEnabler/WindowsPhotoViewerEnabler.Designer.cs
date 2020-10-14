53eea9ef2bedda52a966648fa87cd41d94eea4bc
Sajeeb Chandan <sajeeb4@gmail.com>
2020-10-15 02:28:11 +0600

    Add project files.

---
+++ WindowsPhotoViewerEnabler/WindowsPhotoViewerEnabler.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsPhotoViewerEnabler
+{
+    partial class WindowsPhotoViewerEnabler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WindowsPhotoViewerEnabler));
+            this.buttonEnable = new System.Windows.Forms.Button();
+            this.buttonAbout = new System.Windows.Forms.Button();
+            this.buttonDisable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonEnable
+            // 
+            this.buttonEnable.BackColor = System.Drawing.Color.DarkGreen;
+            this.buttonEnable.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonEnable.FlatAppearance.BorderSize = 0;
+            this.buttonEnable.FlatAppearance.MouseDownBackColor = System.Drawing.Color.ForestGreen;
+            this.buttonEnable.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.buttonEnable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEnable.ForeColor = System.Drawing.Color.White;
+            this.buttonEnable.Location = new System.Drawing.Point(13, 13);
+            this.buttonEnable.Name = "buttonEnable";
+            this.buttonEnable.Size = new System.Drawing.Size(209, 45);
+            this.buttonEnable.TabIndex = 0;
+            this.buttonEnable.Text = "Enable Windows Photo Viewer";
+            this.buttonEnable.UseVisualStyleBackColor = false;
+            this.buttonEnable.Click += new System.EventHandler(this.buttonEnable_Click);
+            // 
+            // buttonAbout
+            // 
+            this.buttonAbout.BackColor = System.Drawing.Color.DarkGreen;
+            this.buttonAbout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonAbout.FlatAppearance.BorderSize = 0;
+            this.buttonAbout.FlatAppearance.MouseDownBackColor = System.Drawing.Color.ForestGreen;
+            this.buttonAbout.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.buttonAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAbout.ForeColor = System.Drawing.Color.White;
+            this.buttonAbout.Location = new System.Drawing.Point(13, 123);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.Size = new System.Drawing.Size(209, 24);
+            this.buttonAbout.TabIndex = 1;
+            this.buttonAbout.Text = "About";
+            this.buttonAbout.UseVisualStyleBackColor = false;
+            this.buttonAbout.Click += new System.EventHandler(this.buttonAbout_Click);
+            // 
+            // buttonDisable
+            // 
+            this.buttonDisable.BackColor = System.Drawing.Color.DarkRed;
+            this.buttonDisable.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonDisable.FlatAppearance.BorderSize = 0;
+            this.buttonDisable.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.buttonDisable.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Brown;
+            this.buttonDisable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDisable.ForeColor = System.Drawing.Color.White;
+            this.buttonDisable.Location = new System.Drawing.Point(13, 64);
+            this.buttonDisable.Name = "buttonDisable";
+            this.buttonDisable.Size = new System.Drawing.Size(209, 45);
+            this.buttonDisable.TabIndex = 2;
+            this.buttonDisable.Text = "Disable Windows Photo Viewer";
+            this.buttonDisable.UseVisualStyleBackColor = false;
+            this.buttonDisable.Click += new System.EventHandler(this.buttonDisable_Click);
+            // 
+            // WindowsPhotoViewerEnabler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(234, 159);
+            this.Controls.Add(this.buttonDisable);
+            this.Controls.Add(this.buttonAbout);
+            this.Controls.Add(this.buttonEnable);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "WindowsPhotoViewerEnabler";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WindowsPhotoViewerEnabler";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEnable;
+        private System.Windows.Forms.Button buttonAbout;
+        private System.Windows.Forms.Button buttonDisable;
+    }
+}
+
---
+++ WindowsPhotoViewerEnabler/WindowsPhotoViewerEnabler.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsPhotoViewerEnabler
+{
+    public partial class WindowsPhotoViewerEnabler : Form
+    {
+        private readonly string enable = @"Windows Registry Editor Version 5.00
+
+; created by Walter Glenn
+; for How-To Geek
+; article: https://www.howtogeek.com/225844/how-to-make-windows-photo-viewer-your-default-image-viewer-on-windows-10/
+
+[-HKEY_CLASSES_ROOT\Applications\photoviewer.dll]
+
+"";
+        private string disable = @""Windows Registry Editor Version 5.00
+
+; created by Walter Glenn
+; for How-To Geek
+; article: https://www.howtogeek.com/225844/how-to-make-windows-photo-viewer-your-default-image-viewer-on-windows-10/
+
+[HKEY_CLASSES_ROOT\Applications\photoviewer.dll]
+
+[HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell]
+
+[HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open]
+""MuiVerb""=""@photoviewer.dll,-3043""
+
+[HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\command]
+@=hex(2):25,00,53,00,79,00,73,00,74,00,65,00,6d,00,52,00,6f,00,6f,00,74,00,25,\
+00,5c,00,53,00,79,00,73,00,74,00,65,00,6d,00,33,00,32,00,5c,00,72,00,75,00,\
+6e,00,64,00,6c,00,6c,00,33,00,32,00,2e,00,65,00,78,00,65,00,20,00,22,00,25,\
+00,50,00,72,00,6f,00,67,00,72,00,61,00,6d,00,46,00,69,00,6c,00,65,00,73,00,\
+25,00,5c,00,57,00,69,00,6e,00,64,00,6f,00,77,00,73,00,20,00,50,00,68,00,6f,\
+00,74,00,6f,00,20,00,56,00,69,00,65,00,77,00,65,00,72,00,5c,00,50,00,68,00,\
+6f,00,74,00,6f,00,56,00,69,00,65,00,77,00,65,00,72,00,2e,00,64,00,6c,00,6c,\
+00,22,00,2c,00,20,00,49,00,6d,00,61,00,67,00,65,00,56,00,69,00,65,00,77,00,\
+5f,00,46,00,75,00,6c,00,6c,00,73,00,63,00,72,00,65,00,65,00,6e,00,20,00,25,\
+00,31,00,00,00
+
+[HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\DropTarget]
+""Clsid""=""{FFE2A43C-56B9-4bf5-9A79-CC6D4285608A}""
+
+[HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print]
+
+[HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\command]
+@=hex(2):25,00,53,00,79,00,73,00,74,00,65,00,6d,00,52,00,6f,00,6f,00,74,00,25,\
+00,5c,00,53,00,79,00,73,00,74,00,65,00,6d,00,33,00,32,00,5c,00,72,00,75,00,\
+6e,00,64,00,6c,00,6c,00,33,00,32,00,2e,00,65,00,78,00,65,00,20,00,22,00,25,\
+00,50,00,72,00,6f,00,67,00,72,00,61,00,6d,00,46,00,69,00,6c,00,65,00,73,00,\
+25,00,5c,00,57,00,69,00,6e,00,64,00,6f,00,77,00,73,00,20,00,50,00,68,00,6f,\
+00,74,00,6f,00,20,00,56,00,69,00,65,00,77,00,65,00,72,00,5c,00,50,00,68,00,\
+6f,00,74,00,6f,00,56,00,69,00,65,00,77,00,65,00,72,00,2e,00,64,00,6c,00,6c,\
+00,22,00,2c,00,20,00,49,00,6d,00,61,00,67,00,65,00,56,00,69,00,65,00,77,00,\
+5f,00,46,00,75,00,6c,00,6c,00,73,00,63,00,72,00,65,00,65,00,6e,00,20,00,25,\
+00,31,00,00,00
+
+[HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\DropTarget]
+""Clsid""=""{60fd46de-f830-4894-a628-6fa81bc0190d}""";
+        private readonly string disable = @"Windows Registry Editor Version 5.00
+
+; created by Walter Glenn
+; for How-To Geek
+; article: https://www.howtogeek.com/225844/how-to-make-windows-photo-viewer-your-default-image-viewer-on-windows-10/
+
+[-HKEY_CLASSES_ROOT\Applications\photoviewer.dll]
+
+";
+        public WindowsPhotoViewerEnabler()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEnable_Click(object sender, EventArgs e)
+        {
+            string myTempFile = Path.Combine(Path.GetTempPath(), "enable.reg");
+            using (StreamWriter sw = new StreamWriter(myTempFile))
+            {
+                sw.Write(enable);
+            }
+            CreateProcess(myTempFile);
+        }
+
+        private void CreateProcess(string filepath)
+        {
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Normal,
+                FileName = "regedit.exe",
+                Arguments = filepath
+            };
+            process.Start();
+            process.OutputDataReceived += RegeditProcess_OutputDataReceived;
+            process.ErrorDataReceived += RegeditProcess_ErrorDataReceived;
+            process.Exited += RegeditProcess_Exited;
+        }
+
+        private void RegeditProcess_Exited(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void RegeditProcess_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            MessageBox.Show(e.Data);
+        }
+
+        private void RegeditProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            MessageBox.Show(e.Data);
+        }
+
+        private void buttonDisable_Click(object sender, EventArgs e)
+        {
+            string myTempFile = Path.Combine(Path.GetTempPath(), "disable.reg");
+            using (StreamWriter sw = new StreamWriter(myTempFile))
+            {
+                sw.Write(disable);
+            }
+            CreateProcess(myTempFile);
+        }
+
+        private void buttonAbout_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
